d0231a905d657d4a74e906e70f9a103b5f610601
HalaGR <91056497+HalaGR@users.noreply.github.com>
2021-11-11 00:52:47 +0200

    update main-main

--- main-main.tf
+++ main-main.tf
@@ -9,11 +9,34 @@ terraform {
 
 provider "aws" {
   region = "eu-west-1"
-  //access key values
+  access_key = "AKIATTDC4KMEMCZZRTWK" // add your own key valuse here
+  secret_key = "8nykO0/YW7FqE9t98/Y5HkrhA2Zoog+DV0dFNZiL"
 }
 
 module "S3" {
     source = ".//modules/S3/"
     #bucket name should be unique
     bucket_name = "final-task-s3"       
+}
+
+module "vpc" {
+  source = ".//modules/vpc/"
+  vpc_name = "default-vpc"
+}
+
+
+module "web-app-az-1" {
+  source = ".//modules/instance/"
+
+  instance_type = "t2.micro"
+  availability_zone = "eu-west-1c"
+  instance_name_tag = "web-server-"
+}
+
+module "web-apppaz-2" {
+  source = ".//modules/instance/"
+
+  instance_type = "t2.micro"
+  availability_zone = "eu-west-1b"
+  instance_name_tag = "web-server-"
 }
